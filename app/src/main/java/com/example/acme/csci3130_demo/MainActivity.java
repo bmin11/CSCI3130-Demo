920cfeb4ccb8a44166b4d241649e9006a2ac547a
bmin11 <br239881@gmail.com>
2018-01-19 15:25:45 -0400

    Assignment 1

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btCopy;
+    private EditText tbHello, tbHello2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btCopy = findViewById(R.id.btCopy);
+        tbHello = findViewById(R.id.tbHello);
+        tbHello2 = findViewById(R.id.tbHello2);
+
+        btCopy.setOnClickListener(new View.OnClickListener() {
+            public  void onClick(View v) {
+                tbHello2.setText(tbHello.getText());
+            }
+        });
     }
 }
